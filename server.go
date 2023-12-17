578787e679a6f70c94b1a14172168c6937e97ed4
Fr0s1 <dthieu223@gmail.com>
2023-12-17 20:07:28 +0700

    Initial commit

---
+++ server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/Fr0s1/howtographql-go-hackernews/graph"
+	"github.com/Fr0s1/howtographql-go-hackernews/internal/auth"
+	database "github.com/Fr0s1/howtographql-go-hackernews/internal/pkg/db/mysql"
+	"github.com/go-chi/chi/v5"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(auth.Middleware())
+
+	database.InitDB()
+
+	defer database.CloseDB()
+
+	database.Migrate()
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
